347192510a14b65402dbd67c4651ee4fce37abe4
mik284 <mikeykamau222@gmail.com>
2023-01-09 17:40:00 +0300

    Added a simple validation

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,17 +1,36 @@
 module.exports = {
+    //@route /api/users
     registerUsers: async (req, res) => {
         try {
-            res.status(201).json({
-                message: "registered succefully"
-            })
+            const {
+                username,
+                email,
+                passsword
+            } = req.body
+
+            //@validation
+            if (!username || !email || !passsword) {
+                res.status(400).json({
+                    message: "Please include all the empty fields"
+                })
+            } else {
+
+                res.status(201).json({
+                    message: "registered succefully"
+                })
+            }
+
         } catch (error) {
             res.status(401).json({
                 message: error.message
             })
         }
     },
+
+    //@route /api/users/login
     loginUsers: async (req, res) => {
         try {
+
             res.status(201).json({
                 message: "login user succefully"
             })
--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const userRoutes = require('./routes/userRouters')
 
 dotenv.config()
 const app = express()
+app.use(express.json())// to allow sending of data in raw json format
+app.use(express.urlencoded({extended: false})) //to accept url encoded form 
 
 app.use('/api/users', userRoutes)
 
