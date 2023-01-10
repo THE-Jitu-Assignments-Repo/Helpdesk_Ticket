a7b5eb2815f952dd98275f0589729c4bfd8f49d2
mik284 <mikeykamau222@gmail.com>
2023-01-10 10:46:38 +0300

    updated register users controller, added bcrypt and made conn to db

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,8 +1,10 @@
 const dotenv = require('dotenv').config()
+const bcrypt = require('bcrypt')
+const User = require('../models/userModel')
 
 module.exports = {
     //@route /api/users
-    registerUsers: async(req, res) => {
+    registerUsers: async (req, res) => {
         try {
             const {
                 username,
@@ -12,17 +14,48 @@ module.exports = {
             // console.log(username, email, password);
             //@validation
             if (!username || !email || !password) {
-                // res.status(400).json({
-                //     message: "Please include all the empty fields"
-                // })
                 res.status(400)
                 throw new Error('Please include all the empty fields')
-            } else{
-                res.status(201).json({
-                    message: "registered succefully"
+            } else {
+
+
+                // Find if the user exists
+                const userExist = await User.findOne({
+                    email,
+                    password
                 })
 
-            }            
+                if (userExist) {
+
+                    res.status(400)
+                    throw new Error('Invalid credentials')
+                }
+
+                // Hash password
+
+                const hashedPassword = await bcrypt.hash(password, process.env.SALT)
+
+
+                // Create user
+                const user = await User.create({
+                    username,
+                    email,
+                    password: hashedPassword
+                })
+
+                if (user) {
+                    res.status(201).json({
+                        _id: user._id,
+                        name: user.username,
+                        email: user.email,
+                        message: "registered succefully"
+                    })
+                }else{
+                    res.status(400).json({
+                        message: "Invalid data"
+                    })
+                }
+            }
 
         } catch (error) {
             res.status(401).json({
