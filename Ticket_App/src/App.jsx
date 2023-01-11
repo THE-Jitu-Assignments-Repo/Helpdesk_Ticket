8fd196f4344b96404f0f1e22d97c2758eb3f6d82
mik284 <mikeykamau222@gmail.com>
2023-01-11 09:39:12 +0300

    Created ticket fronted

---
+++ Ticket_App/src/App.jsx
@@ -0,0 +1,12 @@
+import Routers from "./routes/Routers"
+
+function App() {
+
+  return (
+    <div>
+      <Routers />   
+    </div>
+  )
+}
+
+export default App
---
+++ Ticket_App/src/routes/Routers.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Routes, Route} from 'react-router-dom'
+
+function Routers() {
+  return (
+    <div>Routers</div>
+  )
+}
+
+export default Routers
