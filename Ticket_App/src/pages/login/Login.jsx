7ef8a4c76cc5f283a9b1d80a0055a3ae7fdd6a01
mik284 <mikeykamau222@gmail.com>
2023-01-11 13:12:16 +0300

    Worked on login form skeleton

--- Ticket_App/src/pages/login/Login.jsx
+++ Ticket_App/src/pages/login/Login.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import "./login.css";
 
 function Login() {
   return (
-    <div>Login</div>
-  )
+    <div className="login">
+      <span>Login to HelpDesk Tickets</span>
+
+      <form className="form">
+        <input type="email" name="" placeholder="...@gmail.com" />
+        <input type="password" placeholder="Enter password" />
+        <div className="form_button">
+          <button className="btn">Sign in</button>
+        </div>
+        <span className="reg_link">
+          <Link to="/register">Already have an Account!</Link>
+        </span>
+      </form>
+    </div>
+  );
 }
 
-export default Login
+export default Login;
