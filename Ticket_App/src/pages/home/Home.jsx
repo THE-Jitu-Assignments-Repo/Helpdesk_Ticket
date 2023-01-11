26dfb9400ad000e1231944eb13f5dbc82ec1e0c9
mik284 <mikeykamau222@gmail.com>
2023-01-11 14:40:50 +0300

    Worked on home styling and content

--- Ticket_App/src/pages/home/Home.jsx
+++ Ticket_App/src/pages/home/Home.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from "react";
+import { FaQuestionCircle, FaTicketAlt } from "react-icons/fa";
+import { Link } from "react-router-dom";
+import './home.css'
 
 function Home() {
   return (
-    <div>Home</div>
-  )
+    <div className="home">
+      <section className="heading">
+        <h1>What do you need help with?</h1>
+        <p>Please choose from the options below</p>
+      </section>
+
+      <Link className="btn btn-reverse btn-block">
+        <FaQuestionCircle />
+        Create New Ticket
+      </Link>
+      <Link className="btn btn-block">
+        <FaTicketAlt /> Create New Ticket
+      </Link>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
