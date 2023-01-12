cb8e5fbe3e10c018d398022d202cf63e2d3e51b1
mik284 <mikeykamau222@gmail.com>
2023-01-12 11:25:29 +0300

    updated header component

--- Ticket_App/src/components/header/Header.jsx
+++ Ticket_App/src/components/header/Header.jsx
@@ -1,9 +1,21 @@
 import React from "react";
 import { FaSignInAlt, FaSignOutAlt, FaUser } from "react-icons/fa";
-import { Link } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { Link, useNavigate } from "react-router-dom";
+import { logoutUser } from "../../features/Auth/authActions";
+import { reset } from "../../features/Auth/authSlice";
 import "./header.css";
 
 function Header() {
+  const { user } = useSelector((state) => state.auth);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    dispatch(logoutUser());
+    dispatch(reset());
+    navigate("/");
+  };
   return (
     <div className="header">
       <div className="header_tag">
@@ -12,18 +24,29 @@ function Header() {
         </Link>
       </div>
       <ul className="links">
-        <li>
-          <Link to="login">
-            {" "}
-            <FaSignInAlt />
-            Login
-          </Link>
-        </li>
-        <li>
-          <Link to="register">
-            <FaUser /> Register
-          </Link>
-        </li>
+        {user ? (
+          <li>
+            <button className="btn" onClick={handleLogout}>
+              <FaSignOutAlt />
+              logout
+            </button>
+          </li>
+        ) : (
+          <>
+            <li>
+              <Link to="login">
+                {" "}
+                <FaSignInAlt />
+                Login
+              </Link>
+            </li>
+            <li>
+              <Link to="register">
+                <FaUser /> Register
+              </Link>
+            </li>
+          </>
+        )}
       </ul>
     </div>
   );
