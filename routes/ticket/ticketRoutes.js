bf6b1dadd7e16e476ba75f9296597e7e75669629
mik284 <mikeykamau222@gmail.com>
2023-01-13 10:50:18 +0300

    a demo on how to chain routes

--- routes/ticket/ticketRoutes.js
+++ routes/ticket/ticketRoutes.js
@@ -1,13 +1,26 @@
-const { getTickets, createTickets, getSingleTicket, deleteTicket, updateTicket } = require('../../controllers/ticket/ticketControllers')
-const { verifyToken } = require('../../middleware/Auth/authMiddleware')
+const {
+    getTickets,
+    createTickets,
+    getSingleTicket,
+    deleteTicket,
+    updateTicket
+} = require('../../controllers/ticket/ticketControllers')
+const {
+    verifyToken
+} = require('../../middleware/Auth/authMiddleware')
 const router = require('express').Router()
 
 
-router.post('/create',verifyToken,createTickets)
-router.get('/get',verifyToken,getTickets)
-router.get('/:id',verifyToken,getSingleTicket)
-router.delete('/:id',verifyToken,deleteTicket)
-router.put('/:id',verifyToken,updateTicket)
+router.post('/create', verifyToken, createTickets)
+router.get('/get', verifyToken, getTickets)
+router.get('/:id', verifyToken, getSingleTicket)
+router.delete('/:id', verifyToken, deleteTicket)
+router.put('/:id', verifyToken, updateTicket)
 
 
-module.exports=router
+// chaining routes
+
+router.route('/:id').get(verifyToken, getSingleTicket).delete(verifyToken, deleteTicket).put(verifyToken, updateTicket)
+
+
+module.exports = router
