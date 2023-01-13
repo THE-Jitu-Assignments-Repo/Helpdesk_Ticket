fec848738766abb830560c6f44c2fe825b7785a3
mik284 <mikeykamau222@gmail.com>
2023-01-13 10:47:18 +0300

    Created and Updated ticket contrtoller and routes

--- controllers/ticket/ticketControllers.js
+++ controllers/ticket/ticketControllers.js
@@ -1,25 +1,172 @@
 const Ticket = require('../../models/ticketModel/ticketModel')
+const User = require('../../models/userModel')
 
-module.exports={
-     createTickets: async(req,res)=>{
+module.exports = {
+    createTickets: async (req, res) => {
         try {
-            
+            const {
+                product,
+                description
+            } = req.body
+            if (!product || !description) {
+                res.status(400)
+                throw new Error(`Please add a ${!product? "product": !description?"description":"product and description"}`)
+            }
+
+            const user = await User.findById(req.info._id)
+
+            if (!user) {
+                res.status(401).json({
+                    message: "user not Found"
+                })
+            }
+
+            const newTicket = await Ticket.create({
+                product,
+                description,
+                user,
+                status: 'new'
+            })
+
+            res.status(201).json({
+                newTicket,
+                message: "Ticket created successfully"
+            })
         } catch (error) {
-            
+            res.status(401).json({
+                message: error.message,
+                stack: process.env.Node_env === 'production' ? null : error.stack
+            })
         }
     },
-    getTickets: async(req,res)=>{
+    getTickets: async (req, res) => {
         try {
-            
+            const user = await User.findById(req.info._id)
+
+            if (!user) {
+                res.status(401).json({
+                    message: "user not Found"
+                })
+            }
+
+            const tickets = await Ticket.find({
+                user: req.info._id
+            })
+
+            res.status(201).json(
+                tickets
+            )
+
         } catch (error) {
-            
+            res.status(401).json({
+                message: error.message,
+                stack: process.env.Node_env === 'production' ? null : error.stack
+            })
         }
     },
-    updateTickets: async(req,res)=>{
+    getSingleTicket: async (req, res) => {
         try {
-            
+            const user = await User.findById(req.info._id)
+
+            if (!user) {
+                res.status(401).json({
+                    message: "user not Found"
+                })
+            }
+
+            const singleTicket = await Ticket.findById(req.params.id)
+            if (!singleTicket) {
+                res.status(404).json({
+                    message: "Ticket not Found"
+                })
+            }
+
+            if (singleTicket.user.toString() !== req.info._id) {
+                res.status(401)
+                throw new Error('Not authorized')
+            }
+
+            res.status(201).json(
+                singleTicket
+            )
+
         } catch (error) {
-            
+            res.status(401).json({
+                message: error.message,
+                stack: process.env.Node_env === 'production' ? null : error.stack
+            })
         }
-    }    
+    },
+    deleteTicket: async (req, res) => {
+        try {
+            const user = await User.findById(req.info._id)
+
+            if (!user) {
+                res.status(401).json({
+                    message: "user not Found"
+                })
+            }
+
+            const singleTicket = await Ticket.findById(req.params.id)
+            if (!singleTicket) {
+                res.status(404).json({
+                    message: "Ticket not Found"
+                })
+            }
+
+            if (singleTicket.user.toString() !== req.info._id) {
+                res.status(401)
+                throw new Error('Not authorized')
+            }
+
+            await singleTicket.remove()
+
+            res.status(201).json({
+                message: "Deleted"
+            })
+
+        } catch (error) {
+            res.status(401).json({
+                message: error.message,
+                stack: process.env.Node_env === 'production' ? null : error.stack
+            })
+        }
+    },
+    updateTicket: async (req, res) => {
+        try {
+            const user = await User.findById(req.info._id)
+
+            if (!user) {
+                res.status(401).json({
+                    message: "user not Found"
+                })
+            }
+
+            const singleTicket = await Ticket.findById(req.params.id)
+            if (!singleTicket) {
+                res.status(404).json({
+                    message: "Ticket not Found"
+                })
+            }
+
+            if (singleTicket.user.toString() !== req.info._id) {
+                res.status(401)
+                throw new Error('Not authorized')
+            }
+
+            const updatedTicket = await singleTicket.findByIdAndUpdate(req.params.id, req.body, {
+                new: true
+            })
+
+            res.status(201).json(updatedTicket, {
+                message: "updated succefully"
+            })
+
+        } catch (error) {
+            res.status(401).json({
+                message: error.message,
+                stack: process.env.Node_env === 'production' ? null : error.stack
+            })
+        }
+    }   
 }
--- routes/ticket/ticketRoutes.js
+++ routes/ticket/ticketRoutes.js
@@ -1,11 +1,13 @@
-const { getTickets, createTickets } = require('../../controllers/ticket/ticketControllers')
+const { getTickets, createTickets, getSingleTicket, deleteTicket, updateTicket } = require('../../controllers/ticket/ticketControllers')
 const { verifyToken } = require('../../middleware/Auth/authMiddleware')
 const router = require('express').Router()
 
 
 router.post('/create',verifyToken,createTickets)
 router.get('/get',verifyToken,getTickets)
-
+router.get('/:id',verifyToken,getSingleTicket)
+router.delete('/:id',verifyToken,deleteTicket)
+router.put('/:id',verifyToken,updateTicket)
 
 
 module.exports=router
