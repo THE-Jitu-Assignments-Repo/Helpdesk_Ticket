74c0ff0a759efd7625a4c22d157fdbeab6a2a380
mik284 <mikeykamau222@gmail.com>
2023-01-14 09:52:38 +0300

    Added nested routes for private routes

--- Ticket_App/src/routes/Routers.jsx
+++ Ticket_App/src/routes/Routers.jsx
@@ -5,15 +5,20 @@ import Register from "../pages/register/Register";
 import Index from "../components/Layout/Index";
 import Home from "../pages/home/Home";
 import NewTicket from "../pages/Tickets/NewTicket";
+import Private from "../components/Layout/Guard/Private";
 
 function Routers() {
   return (
     <Routes>
       <Route path="/" element={<Index />}>
-        <Route index element={<Home />}/>
+        <Route index element={<Home />} />
         <Route path="login" element={<Login />} />
         <Route path="register" element={<Register />} />
-        <Route path="newTicket" element={<NewTicket />}/>
+
+        <Route path="/newTicket" element={<Private />}>
+          <Route path="newTicket" element={<NewTicket />} />
+        </Route>
+        
       </Route>
     </Routes>
   );
