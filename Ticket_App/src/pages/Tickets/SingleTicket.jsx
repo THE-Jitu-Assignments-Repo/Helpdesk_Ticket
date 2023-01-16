9ac716803d6aac01deb8c8fa5a1f46df00e78fa7
mik284 <mikeykamau222@gmail.com>
2023-01-16 11:53:50 +0300

    Updated single Ticket page and its styling

--- Ticket_App/src/pages/Tickets/SingleTicket.jsx
+++ Ticket_App/src/pages/Tickets/SingleTicket.jsx
@@ -5,6 +5,8 @@ import { useDispatch } from 'react-redux';
 import { useParams } from 'react-router-dom';
 import { useEffect } from 'react';
 import { getSingleTicket } from '../../features/Tickets/ticketActions';
+import Spinner from '../../components/spinner/Spinner';
+import Back from '../../components/backButton/Back';
 
 function SingleTicket() {
     const {ticket, isLoading, isSuccess, message, isError} = useSelector(state=>state.ticket)
@@ -19,9 +21,33 @@ function SingleTicket() {
         }
 
         dispatch(getSingleTicket(ticketID))
+        //eslint-disable-next-line        
     }, [message, isError,ticketID])
+
+    if(isLoading){
+        return <Spinner />
+    }
+    if(isError){
+        return <h2>Something went wrong</h2>
+    }
   return (
-    <div>SingleTicket</div>
+    <div className="ticket-page">
+        <header className="ticket-head">
+            <Back url='/tickets' />
+            <h2>
+                TIcket ID: {ticket._id}
+                <span className={`status status-${ticket.status}`}>{ticket.status}</span>
+            </h2>
+            <h3>
+                Date Submitted: {new Date(ticket.createdAt).toLocaleString('en-US')}
+            </h3>
+            <hr />
+            <div className="ticket-desc">
+                <h3>Description of the issue</h3>
+                    <p>{ticket.description}</p>
+            </div>
+        </header>
+    </div>
   )
 }
 
