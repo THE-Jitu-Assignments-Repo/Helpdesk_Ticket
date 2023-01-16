bb89fbd6945cdae55b1bd1bb71a23dff42cf0283
mik284 <mikeykamau222@gmail.com>
2023-01-16 11:29:21 +0300

    Modified single ticket page

--- Ticket_App/src/pages/Tickets/SingleTicket.jsx
+++ Ticket_App/src/pages/Tickets/SingleTicket.jsx
@@ -1,6 +1,25 @@
 import React from 'react'
+import { useState } from 'react';
+import { useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
+import { useParams } from 'react-router-dom';
+import { useEffect } from 'react';
+import { getSingleTicket } from '../../features/Tickets/ticketActions';
 
 function SingleTicket() {
+    const {ticket, isLoading, isSuccess, message, isError} = useSelector(state=>state.ticket)
+    const [err, setErr] = useState('')
+    const dispatch = useDispatch()
+
+    const {ticketID} = useParams()
+
+    useEffect(()=>{
+        if(isError){
+            setErr(message)
+        }
+
+        dispatch(getSingleTicket(ticketID))
+    }, [message, isError,ticketID])
   return (
     <div>SingleTicket</div>
   )
