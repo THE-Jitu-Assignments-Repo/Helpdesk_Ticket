21d2335aa1661c8568f9dc4250eb1512b858a1c9
mik284 <mikeykamau222@gmail.com>
2023-01-23 19:04:16 +0300

    modified register page

--- Ticket_App/src/pages/register/Register.jsx
+++ Ticket_App/src/pages/register/Register.jsx
@@ -60,54 +60,59 @@ useEffect(()=>{
     dispatch(reset())
   }
 
-  {isLoading ? <Spinner /> : (
-    <div className="register">
-      <FaUser size={40} />
-      <span>Register to HelpDesk Tickets</span>
 
-      <form className="form" onSubmit={registerUser}>
-        {err? <span style={{color:'red', display:'flex', alignItems:'center', gap:'4px'}}><FaWindowClose /> {err}</span>: ''}
-        <input
-          type="text"
-          name="username"
-          placeholder="eg. johnDoe"
-          value={username}
-          onChange={handleChange}
-        />
-        <input
-          type="email"
-          name="email"
-          placeholder="...@gmail.com"
-          value={email}
-          onChange={handleChange}
-        />
-        <input
-          type="password"
-          name="password"
-          placeholder="Enter password"
-          value={password}
-          onChange={handleChange}
-        />
-        <input
-          type="password"
-          name="confirmed_password"
-          placeholder="Confirm password"
-          value={confirmed_password}
-          onChange={handleChange}
-        />
-
-        <div className="form_button">
-          <button className="btn">Register</button>
+return (
+  <>
+    {isLoading ? <Spinner /> : 
+      (
+        <div className="register">
+          <FaUser size={40} />
+          <span>Register to HelpDesk Tickets</span>
+    
+          <form className="form" onSubmit={registerUser}>
+            {err? <span style={{color:'red', display:'flex', alignItems:'center', gap:'4px'}}><FaWindowClose /> {err}</span>: ''}
+            <input
+              type="text"
+              name="username"
+              placeholder="eg. johnDoe"
+              value={username}
+              onChange={handleChange}
+            />
+            <input
+              type="email"
+              name="email"
+              placeholder="...@gmail.com"
+              value={email}
+              onChange={handleChange}
+            />
+            <input
+              type="password"
+              name="password"
+              placeholder="Enter password"
+              value={password}
+              onChange={handleChange}
+            />
+            <input
+              type="password"
+              name="confirmed_password"
+              placeholder="Confirm password"
+              value={confirmed_password}
+              onChange={handleChange}
+            />
+    
+            <div className="form_button">
+              <button className="btn">Register</button>
+            </div>
+            <span className="reg_link">
+              <Link to="/login">
+                <FaHandPointRight className="handme" />
+                Already have an Account!
+              </Link>
+            </span>
+          </form>
         </div>
-        <span className="reg_link">
-          <Link to="/login">
-            <FaHandPointRight className="handme" />
-            Already have an Account!
-          </Link>
-        </span>
-      </form>
-    </div>
-  );
-}
-
+      )
+    }
+  </>
+)}
 export default Register;
