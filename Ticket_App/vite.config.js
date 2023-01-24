aad10d1c2f38d54a4a20b3acde8e73f979e771ec
mik284 <mikeykamau222@gmail.com>
2023-01-24 15:34:26 +0300

    modified vite config

--- Ticket_App/vite.config.js
+++ Ticket_App/vite.config.js
@@ -2,19 +2,13 @@ import {
   defineConfig
 } from 'vite'
 import react from '@vitejs/plugin-react'
-import babel from 'vite-plugin-babel';
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  jsx: 'babel',
+
   plugins: [
-    react(),
-    ['babel-plugin-import', {
-        'libraryName': 'antd',
-        'libraryDirectory': 'es',
-        'style': 'css'
-    }],
-    ['@babel/plugin-transform-react-jsx', {        pragma: 'h',        pragmaFrag: 'Fragment',        useBuiltIns: true    }]
-]
+    react()
+  ]
 
 })
