1e4086716adea9a2cab4e40fddd15465137abf83
mik284 <mikeykamau222@gmail.com>
2023-01-24 09:21:07 +0300

    modified vite config

--- Ticket_App/vite.config.js
+++ Ticket_App/vite.config.js
@@ -7,6 +7,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   jsx: 'babel',
   plugins: [
+    [react()],
     ['babel-plugin-import', {
       'libraryName': 'antd',
       'libraryDirectory': 'es',
