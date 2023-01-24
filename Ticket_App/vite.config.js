5228871de77045fba418972c7f6255a3f7954c4a
mik284 <mikeykamau222@gmail.com>
2023-01-24 09:26:31 +0300

    modified vite config

--- Ticket_App/vite.config.js
+++ Ticket_App/vite.config.js
@@ -6,18 +6,29 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   jsx: 'babel',
-  plugins: [
-    [react()],
-    ['babel-plugin-import', {
-      'libraryName': 'antd',
-      'libraryDirectory': 'es',
-      'style': 'css'
-    }],
-    ['@babel/plugin-transform-react-jsx', {
-      pragma: 'h',
-      pragmaFrag: 'Fragment',
-      useBuiltIns: true
+   presets: [
+    "@babel/preset-flow",
+    ["@babel/preset-env", {
+      "debug": true,
+      "loose": false,
+      "targets": {
+        "node": true
+      }
     }]
   ],
+  plugins: [[react()],["@babel/plugin-proposal-object-rest-spread", { "loose": true, "useBuiltIns": true }]]
+  // plugins: [
+  //   [react()],
+  //   ['babel-plugin-import', {
+  //     'libraryName': 'antd',
+  //     'libraryDirectory': 'es',
+  //     'style': 'css'
+  //   }],
+  //   ['@babel/plugin-transform-react-jsx', {
+  //     pragma: 'h',
+  //     pragmaFrag: 'Fragment',
+  //     useBuiltIns: true
+  //   }]
+  // ],
 
 })
