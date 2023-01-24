4f10431403d4947f3d3d3f4f6a57d264c6b0b023
mik284 <mikeykamau222@gmail.com>
2023-01-24 07:23:48 +0300

    Modified vite config set for production

--- Ticket_App/vite.config.js
+++ Ticket_App/vite.config.js
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
+import {
+  defineConfig
+} from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  jsx: 'babel',
+  plugins: [
+    'babel-plugin-import',
+    [
+      '@babel/plugin-transform-react-jsx',
+      {
+        pragma: 'h',
+        pragmaFrag: 'Fragment',
+        useBuiltIns: true
+      },
+    ],
+  ],
 })
