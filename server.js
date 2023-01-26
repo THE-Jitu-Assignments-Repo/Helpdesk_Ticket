0a20104790636e8acf9fc66cb4b4f0354b4ebe37
mik284 <mikeykamau222@gmail.com>
2023-01-27 01:20:56 +0300

    Modified severjs for deployment

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const colors = require('colors')
 const dotenv = require('dotenv')
 const userRoutes = require('./routes/user/userRouters')
 const ticketRoutes = require('./routes/ticket/ticketRoutes')
+const path = require('path')
 const cors = require('cors')
 // const {errorHandler} = require('./middleware/Helper/errorHandler')
 const {connectDB} = require('./config/db')
@@ -17,9 +18,14 @@ app.use(express.json())// to allow sending of data in raw json format
 app.use(express.urlencoded({extended: false})) //to accept url encoded form 
 // app.use(errorHandler)
 
+app.use(express.static(path.join(__dirname, 'client/build')))
+
+app.get('*', (req, res)=>{
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'))
+})
 app.use('/api/users', userRoutes)
 app.use('/api/tickets', ticketRoutes)
 
-app.listen(process.env.PORT, ()=>{
+app.listen(process.env.PORT || 3000, ()=>{
     console.log(`listening to port ${process.env.PORT}`);
 })
