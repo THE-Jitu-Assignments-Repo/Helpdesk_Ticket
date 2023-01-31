1fa186aad15421248dd88d596cc52e67d866bfe2
mik284 <mikeykamau222@gmail.com>
2023-01-31 07:18:04 +0300

    Added validator for login and register

---
+++ Ticket_App/src/helper/auth/authvalidator.js
@@ -0,0 +1,20 @@
+import * as yup from 'yup'
+
+
+export const authLoginValidatorSchema = (details)=>{
+    const schema = yup.object().shape({
+        email: yup.string().email().required('Email is reuired'),
+        password: yup.string().min(8, 'Password has to be longer than 8 characters!').required('Password is required!')
+    })
+
+    return schema.validate(details)
+}
+
+export const    authRegisterValidatorSchema = (details)=>{
+    const schema = yup.object().shape({
+        username: yup.string().required('Please include username'),
+        email: yup.string().email().required('Email is reuired'),
+        password: yup.string().min(8, 'Password has to be longer than 8 characters!').required('Password is required!')
+    })
+    return schema.validate(details)
+}
