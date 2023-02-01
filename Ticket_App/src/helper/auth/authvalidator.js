fa3cc8783650865156aa532a970ce815298fe04a
mik284 <mikeykamau222@gmail.com>
2023-02-01 06:19:44 +0300

    Modified the auth validator

--- Ticket_App/src/helper/auth/authvalidator.js
+++ Ticket_App/src/helper/auth/authvalidator.js
@@ -4,7 +4,7 @@ import * as yup from 'yup'
 export const authLoginValidatorSchema = (details)=>{
     const schema = yup.object().shape({
         email: yup.string().email().required('Email is reuired'),
-        password: yup.string().min(8, 'Password has to be longer than 8 characters!').required('Password is required!')
+        password: yup.string().required('Password is required!')
     })
 
     return schema.validate(details)
