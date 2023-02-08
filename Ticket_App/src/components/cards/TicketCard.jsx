8560cd4f7442eb279a88a577786fd268e8c734fd
mik284 <mikeykamau222@gmail.com>
2023-02-08 04:13:14 +0300

    update media query and ticket component

--- Ticket_App/src/components/cards/TicketCard.jsx
+++ Ticket_App/src/components/cards/TicketCard.jsx
@@ -1,7 +1,10 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { useDispatch } from 'react-redux';
 
 function TicketCard({ item }) {
+  const navigate = useNavigate()
+  const dispatch = useDispatch()
   let date = new Date(item.createdAt);
   let day = date.getDate();
   let month = date.getMonth() + 1; // getMonth returns a zero-based value, so we add 1
@@ -9,11 +12,11 @@ function TicketCard({ item }) {
   let displayDate = `${month}/${day}/${year}`;
   // console.log(item);
   return (
-    <div className="ticket">
+    <div className="ticket" onClick={()=>navigate(`/tickets/${item._id}`)}>
       <div className="ticket--date">
         {displayDate}
       </div>
-      <div className="ticket--product--name">{item.product}</div>
+      <div className="ticket--product--name" >{item.product}</div>
       <div className={`status status-${item.status}`}>{item.status}</div>
       <Link
         to={`/tickets/${item._id}`}
