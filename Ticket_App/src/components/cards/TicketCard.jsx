e98efe078ef6cd6080f1adca0645366ace4ae15b
mik284 <mikeykamau222@gmail.com>
2023-02-08 03:55:28 +0300

    update media query

--- Ticket_App/src/components/cards/TicketCard.jsx
+++ Ticket_App/src/components/cards/TicketCard.jsx
@@ -5,10 +5,10 @@ function TicketCard({item}) {
     // console.log(item);
   return (
     <div className="ticket">
-        <div>{new Date(item.createdAt).toLocaleString('en-US')}</div>
-        <div>{item.product}</div>
+        <div className='ticket--date'>{new Date(item.createdAt).toLocaleString('en-US')}</div>
+        <div className='ticket--product--name'>{item.product}</div>
         <div className={`status status-${item.status}`}>{item.status}</div>
-        <Link to={`/tickets/${item._id}`} className='btn btn-reverse btn-sm'>
+        <Link to={`/tickets/${item._id}`} className='btn btn-reverse btn-sm' id='view'>
             view
         </Link>
     </div>
