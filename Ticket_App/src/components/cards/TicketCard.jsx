71edf826bf76e03d5ea8c3a60c7460cd42012497
mik284 <mikeykamau222@gmail.com>
2023-02-08 04:01:57 +0300

    modified date format

--- Ticket_App/src/components/cards/TicketCard.jsx
+++ Ticket_App/src/components/cards/TicketCard.jsx
@@ -1,19 +1,29 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 
-function TicketCard({item}) {
-    // console.log(item);
+function TicketCard({ item }) {
+  let date = new Date(item.createdAt);
+  let day = date.getDate();
+  let month = date.getMonth() + 1; // getMonth returns a zero-based value, so we add 1
+  let year = date.getFullYear();
+  let displayDate = `${month}/${day}/${year}`;
+  // console.log(item);
   return (
     <div className="ticket">
-        <div className='ticket--date'>{new Date(item.createdAt).toLocaleString('en-US')}</div>
-        <div className='ticket--product--name'>{item.product}</div>
-        <div className={`status status-${item.status}`}>{item.status}</div>
-        <Link to={`/tickets/${item._id}`} className='btn btn-reverse btn-sm' id='view'>
-            view
-        </Link>
+      <div className="ticket--date">
+        {displayDate}
+      </div>
+      <div className="ticket--product--name">{item.product}</div>
+      <div className={`status status-${item.status}`}>{item.status}</div>
+      <Link
+        to={`/tickets/${item._id}`}
+        className="btn btn-reverse btn-sm"
+        id="view"
+      >
+        view
+      </Link>
     </div>
-  
-  )
+  );
 }
 
-export default TicketCard
+export default TicketCard;
