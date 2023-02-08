0df59c7a202827f509d57489c1a34e9e89ad2686
mik284 <mikeykamau222@gmail.com>
2023-02-08 03:06:32 +0300

    Add tooltip

--- Ticket_App/src/components/avatar/Avatar.jsx
+++ Ticket_App/src/components/avatar/Avatar.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import Avvvatars from 'avvvatars-react'
+import Tooltip from 'rc-tooltip'
+import "rc-tooltip/assets/bootstrap.css";
 
 
 function Avatar({email}) {
     return (
         <div>
-            <Avvvatars value={email} />
+            <Tooltip placement="bottom" overlay={email}>
+            <span>{' '}<Avvvatars value={email} /></span>
+            </Tooltip>
         </div>
     )
 }
