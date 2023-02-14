e904f139d21dfd6940be11dd18ac42b4ac2f5d12
mik284 <mikeykamau222@gmail.com>
2023-02-14 10:40:23 +0300

    Updated note card

--- Ticket_App/src/components/cards/noteCard.jsx
+++ Ticket_App/src/components/cards/noteCard.jsx
@@ -1,22 +1,33 @@
 import React from "react";
 import { useSelector } from "react-redux";
+import Avatar from "../avatar/Avatar";
 
 function NoteCard({ note }) {
   const { user } = useSelector((state) => state.auth);
   return (
-    <div
-      className="note"
-      style={{
-        backgroundColor: note.isStaff ? "rgba(0,0,0,0.7)" : "#fff",
-        color: note.isStaff ? "#fff" : "#000",
-      }}
-    >
-      <h4>Note from { note.isStaff ? <span>Staff</span>: <span>{user.name}</span>}</h4>
-      <p>{note.text}</p>
-      <div className="note-date">
-        {new Date(note.createdAt).toLocaleString('en-US')}
+    <>
+      <div
+        className="note"
+        style={{
+          backgroundColor: note.isStaff ? "rgba(0,0,0,0.7)" : "#fff",
+          color: note.isStaff ? "#fff" : "#000",
+        }}
+      >
+          <Avatar email={note.isStaff? 'S' : user.email} />
+        <section>
+          <div className="note--header">
+            <h4>
+              Note from{" "}
+              {note.isStaff ? <span>Staff</span> : <span>{user.name} </span>}
+            </h4>
+            <div className="note-date">
+              {new Date(note.createdAt).toLocaleString("en-US")}
+            </div>
+          </div>
+          <p>{note.text}</p>
+        </section>
       </div>
-    </div>
+    </>
   );
 }
 
